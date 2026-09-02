f5645937c6ce3162db79b10ad3fa3b17fa7e4f1e
Lily <lily@sometgirl.online>
2026-09-02 17:15:15 +1000

    meow

--- mailserver/run.sh
+++ mailserver/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Runner script so i dont need to change settings on the LXC that runs this to update,all i need to do is reboot the lxc
-touch /tmp/ItWorked2
-uvicorn main:app --reload --port 8000 --host 0.0.0.0
+uvicorn main:app --reload --port 8000 --host 0.0.0.0 --timeout-keep-alive 30
