218af49af435f37ace81224c2d24368b7473f94e
Lily <lily@sometgirl.online>
2026-09-02 16:36:50 +1000

    network stuffs

--- mailserver/run.sh
+++ mailserver/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Runner script so i dont need to change settings on the LXC that runs this to update,all i need to do is reboot the lxc
-uvicorn main:app --reload --port 8000
+uvicorn main:app --reload --port 8000 --host 0.0.0.0
