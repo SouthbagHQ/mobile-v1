b71cc552af01672d344a08d98f5841d015df9545
Lily <lily@sometgirl.online>
2026-09-02 16:41:32 +1000

    can i die

--- mailserver/run.sh
+++ mailserver/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Runner script so i dont need to change settings on the LXC that runs this to update,all i need to do is reboot the lxc
-touch /tmp/ItWorked
+touch /tmp/ItWorked2
 uvicorn main:app --reload --port 8000 --host 0.0.0.0
